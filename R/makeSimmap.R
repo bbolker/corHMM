1aa4234c95cabf6c993a4c6f33c57fae963147f8
Ben Bolker <bbolker@gmail.com>
2022-04-20 10:43:45 -0400

    remove weird hyphens; robustify makeSimmap

--- R/makeSimmap.R
+++ R/makeSimmap.R
@@ -43,16 +43,17 @@ makeSimmap <- function(tree, data, model, rate.cat, root.p="yang", nSim=1, nCore
   }else{
     StateNames <- legend
   }
-  for(i in 1:nSim){
+  for(i in 1:nSim) {
     tree.simmap <- tree
     tree.simmap$maps <- maps[[i]]
     tree.simmap$maps <- lapply(maps[[i]], function(x) correctMapName(x, StateNames))
     tree.simmap$mapped.edge <- mapped.edge[[i]]
-    colnames(tree.simmap$mapped.edge) <- StateNames
+    ## some states may be completely missing from the tree
+    colnames(tree.simmap$mapped.edge) <- StateNames[as.numeric(colnames(tree.simmap$mapped.edge))]
     tree.simmap$Q <- model
     colnames(tree.simmap$Q) <- rownames(tree.simmap$Q) <- StateNames
     attr(tree.simmap, "map.order") <- "right-to-left"
-    if (!inherits(tree.simmap, "simmap")) 
+    if (!inherits(tree.simmap, "simmap"))
       class(tree.simmap) <- c("simmap", setdiff(class(tree.simmap), "simmap"))
     obj[[i]] <- tree.simmap
   }
@@ -72,7 +73,7 @@ correctMapName <- function(map_element, state_names){
 #   # sample the root state
 #   anc.index <- phy$edge[cladewise.index[1],1]
 #   state.sample[anc.index,] <- t(rmultinom(1, 1, state.probability[anc.index,]))
-#   
+#
 #   # sample the nodes
 #   for(i in cladewise.index){
 #     # A = the probability of transition FROM node i (ancestor)
@@ -264,7 +265,7 @@ getConditionalNodeLik <- function(tree, data, model, rate.cat, root.p, parsimony
     root.p <- liks[focal, ]
   }
   liks[focal, ] <-  liks[focal, ] * root.p
-  
+
   return(list(tip.states = liks[1:nb.tip,],
               node.states = liks[(nb.tip+1):(nb.node+nb.tip),]))
 }
@@ -363,7 +364,7 @@ getSimmapLik <- function(simmap, Q){
   return(lik)
 }
 
-# The Floyd–Warshall algorithm (also known as Floyd's algorithm, the Roy–Warshall algorithm, the Roy–Floyd algorithm, or the WFI algorithm) is an algorithm for finding shortest paths in a directed weighted graph with positive or negative edge weights (but with no negative cycles).
+# The Floyd-Warshall algorithm (also known as Floyd's algorithm, the Roy-Warshall algorithm, the Roy-Floyd algorithm, or the WFI algorithm) is an algorithm for finding shortest paths in a directed weighted graph with positive or negative edge weights (but with no negative cycles).
 FloydWalshAlg <- function(model, init, final){
   nStates <- dim(model)[1]
   Dist <- matrix(Inf, nStates, nStates)
@@ -401,8 +402,8 @@ FloydWalshAlg <- function(model, init, final){
   return(path)
 }
 
-# simmap <- makeSimmap(tree=phy, tip.states=tip.states, states=states, model=model, 
+# simmap <- makeSimmap(tree=phy, tip.states=tip.states, states=states, model=model,
 #                      nSim=10, nCores=1)
-# 
+#
 # liks <- unlist(lapply(simmap, function(x) getSimmapLik(x, Q)))
 # log(sum(exp(liks)))
