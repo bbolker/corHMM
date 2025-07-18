07cb518e1851b413405eb0caf0e907db0a598583
Ben Bolker <bbolker@gmail.com>
2025-07-18 14:31:47 -0400

    test cleanup

--- R/corHMMDredge.R
+++ R/corHMMDredge.R
@@ -153,7 +153,8 @@ corHMMDredgeSA <- function(phy, data, max.rate.cat=1, init.rate.cat=1,
   lewis.asc.bias = FALSE, collapse = FALSE, lower.bound = 1e-10, 
   upper.bound = 100, opts=NULL, verbose=TRUE, p=NULL, rate.cat=NULL, grad=FALSE, 
   max.iterations = 200, initial.temp = 2, cooling.rate = 0.95, 
-  temp.schedule = "exponential", seed = NULL, return.all=FALSE) {
+  temp.schedule = "exponential", seed = NULL, return.all=FALSE,
+  rate.mat = NULL) {
   
   if((is.null(p) & !is.null(rate.cat))){
     print("A rate category was given without specifying a parameter vector (p)")
